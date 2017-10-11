26f6ad9e7f1a3cbbf864315dd8ba592f39cd4c69
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-10-11 14:33:04 +0530

    prettier messages

--- cli.js
+++ cli.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
-var isPublic = require('./index.js')
+const isPublic = require('./index.js')
+const { info, warn, error } = require('prettycli')
 
-var arguments = process.argv
-var repo = arguments[2]
+const repo = process.argv[2]
 
 if (!repo) console.log('Please pass the username/repo')
 else {
   isPublic(repo)
-    .then(public => console.log(`${repo} is${public ? '' : ' not'} public`))
-    .catch(error => console.log(error))
+    .then(public => {
+      if (public) info('PUBLIC', `${repo} is public`)
+      else warn(`${repo} is not public`)
+    })
+    .catch(err => error(err, { silent: true }))
 }
